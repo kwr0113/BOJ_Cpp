0fae0948f25c8ca9c1dfd4700cac071087e3633b
kwr0113 <kwr0113@gmail.com>
2021-09-15 15:45:45 +0900

    chap02

---
+++ chap02/2884.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int h, m;
+    cin >> h >> m;
+    m -= 45;
+    if(m < 0) h -= 1, m += 60;
+    if(h < 0) h =23;
+    cout << h << " " << m << endl;
+}
